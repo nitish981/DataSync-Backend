77ce1fcdfbb8d092880354d0639e1e3927c91875
nitish981 <112499367+nitish981@users.noreply.github.com>
2026-01-05 18:58:44 +0530

    Create auth.js

---
+++ src/auth.js
@@ -0,0 +1,43 @@
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const pool = require('./db');
+
+passport.use(
+  new GoogleStrategy(
+    {
+      clientID: process.env.GOOGLE_CLIENT_ID,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback'
+    },
+    async (accessToken, refreshToken, profile, done) => {
+      try {
+        const email = profile.emails[0].value;
+
+        const result = await pool.query(
+          `INSERT INTO users (email)
+           VALUES ($1)
+           ON CONFLICT (email) DO UPDATE SET email = EXCLUDED.email
+           RETURNING id`,
+          [email]
+        );
+
+        return done(null, {
+          id: result.rows[0].id,
+          email
+        });
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, user);
+});
+
+passport.deserializeUser((user, done) => {
+  done(null, user);
+});
+
+module.exports = passport;
